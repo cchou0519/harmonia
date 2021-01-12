0202e193a08acc17b119d118c27d0255e50e44ba
cchou0519 <73981474+cchou0519@users.noreply.github.com>
2021-01-12 16:03:11 +0800

    Update create_gitea_sh.py

--- examples/docker_deploy_custom/create_gitea_sh.py
+++ examples/docker_deploy_custom/create_gitea_sh.py
@@ -7,7 +7,8 @@ ff.close()
 if edgeNum < 1:
     edgeNum = 1
 
-
+# Create admin
+# Create users
 text = '# Create admin\n\n' \
        'gitea admin user create --admin --username gitea --password password --email admin@admin.com\n\n' \
        '# Create users\n\n' \
@@ -17,12 +18,12 @@ text = '# Create admin\n\n' \
 for i in range(1, edgeNum+1):
     text += 'gitea admin user create --username edge{num} --password 1qaz_WSX --email edge{num}@edge{num}.com --must-change-password=false\n\n'.format(num=i)
 
+# Create repositories
 text += '# Create repositories\n\n' \
         'curl -X POST -H "accept: application/json" -H "Content-Type: application/json" -d \'{"name": "train-plan", "auto_init": true}\' ' \
         'http://gitea:password@127.0.0.1:3000/api/v1/user/repos\n\n' \
         'curl -X POST -H "accept: application/json" -H "Content-Type: application/json" -d \'{"name": "global-model", "auto_init": true}\' ' \
         'http://gitea:password@127.0.0.1:3000/api/v1/user/repos\n\n'
-# print(text)
 
 for i in range(1, edgeNum+1):
     text += 'curl -X POST -H "accept: application/json" -H "Content-Type: application/json" -d \'{"name": "local-model' \
@@ -49,18 +50,15 @@ for i in range(1, edgeNum+1):
     text += 'curl -X PUT -H "accept: application/json" -H "Content-Type: application/json" -d \'{"permission": "read"}\' ' \
             'http://gitea:password@127.0.0.1:3000/api/v1/repos/gitea/global-model/collaborators/edge' + str(i) + '\n\n'
 
-# print(text)
 # local-models
 for i in range(1, edgeNum+1):
     text += '# local-model' + str(i) + '\n\n' \
             'curl -X PUT -H "accept: application/json" -H "Content-Type: application/json" -d \'{"permission": "read"}\' ' \
-            'http://gitea:password@127.0.0.1:3000/api/v1/repos/gitea/local-model1/collaborators/aggregator\n\n' \
+            'http://gitea:password@127.0.0.1:3000/api/v1/repos/gitea/local-model' + str(i) + '/collaborators/aggregator\n\n' \
             'curl -X PUT -H "accept: application/json" -H "Content-Type: application/json" -d \'{"permission": "write"}\' ' \
-            'http://gitea:password@127.0.0.1:3000/api/v1/repos/gitea/local-model1/collaborators/edge' + str(i) + '\n\n' \
+            'http://gitea:password@127.0.0.1:3000/api/v1/repos/gitea/local-model' + str(i) + '/collaborators/edge' + str(i) + '\n\n' \
             'curl -X PUT -H "accept: application/json" -H "Content-Type: application/json" -d \'{"permission": "read"}\' ' \
-            'http://gitea:password@127.0.0.1:3000/api/v1/repos/gitea/local-model1/collaborators/logserver\n\n'
-
-# print(text)
+            'http://gitea:password@127.0.0.1:3000/api/v1/repos/gitea/local-model' + str(i) + '/collaborators/logserver\n\n'
 
 # Create webhooks
 # train-plan
@@ -72,8 +70,6 @@ for i in range(1, edgeNum+1):
     text += 'curl -X POST -H "accept: application/json" -H "Content-Type: application/json" ' \
             '-d \'{"active": true, "config": {"content_type": "json", "url": "http://edge' + str(i) + ':9080"}, "events": ["push"], "type": "gitea"}\' ' \
             'http://gitea:password@127.0.0.1:3000/api/v1/repos/gitea/train-plan/hooks\n\n'
-
-# print(text)
 
 # global-model
 text += '# global-model\n\n' \

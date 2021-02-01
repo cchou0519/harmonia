d29bbced3df47f5864bca2dd351e32a5ef8a418e
cchou0519 <73981474+cchou0519@users.noreply.github.com>
2021-02-01 01:37:20 -0800

    Update setup.py

--- examples/edge/setup.py
+++ examples/edge/setup.py
@@ -3,7 +3,7 @@ setup(
     name='blurnn',
     version='0.1',
     description='BlurNN is a pytorch privacy preserving module',
-    packages=['blurnn', 'blurnn.optim'],
+    packages=['/app/blurnn', '/app/blurnn.optim'],
     author='AILabs',
     license='MIT License',
     requires=['torch'],
